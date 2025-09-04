836b935748a3b1281c2eb13360a896f56b2ac95e
nguyenhuutam-vt <nguyenvinhspkt10@gmail.com>
2025-09-04 13:32:45 +0700

    init code

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
+const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <App />
+    </QueryClientProvider>
     <ToastContainer
       position="top-right"
       autoClose={5000}
@@ -20,5 +26,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       pauseOnHover
       theme="light"
     />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
